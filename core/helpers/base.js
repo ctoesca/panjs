847ff908ea600e49f82c36c36e3c25156ca18f61
ctoesca <christophe.toesca@nexilearn.fr>
2015-07-08 22:23:55 +0200

    remove IE8 extensions

--- core/helpers/base.js
+++ core/helpers/base.js
@@ -5,99 +5,6 @@ String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-
-// Add ECMA262-5 method binding if not supported natively
-//
-if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
-            return function() {
-                return that.apply(owner, arguments);
-            };
-        } else {
-            var args= Array.prototype.slice.call(arguments, 1);
-            return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
-            };
-        }
-    };
-}
-
-// Add ECMA262-5 string trim if not supported natively
-//
-if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
-        return this.replace(/^\s+/, '').replace(/\s+$/, '');
-    };
-}
-
-// Add ECMA262-5 Array methods if not supported natively
-//
-if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (i in this && this[i]===find)
-                return i;
-        return -1;
-    };
-}
-if (!('lastIndexOf' in Array.prototype)) {
-    Array.prototype.lastIndexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= this.length-1;
-        if (i<0) i+= this.length;
-        if (i>this.length-1) i= this.length-1;
-        for (i++; i-->0;) /* i++ because from-argument is sadly inclusive */
-            if (i in this && this[i]===find)
-                return i;
-        return -1;
-    };
-}
-if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this)
-                action.call(that, this[i], i, this);
-    };
-}
-if (!('map' in Array.prototype)) {
-    Array.prototype.map= function(mapper, that /*opt*/) {
-        var other= new Array(this.length);
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this)
-                other[i]= mapper.call(that, this[i], i, this);
-        return other;
-    };
-}
-if (!('filter' in Array.prototype)) {
-    Array.prototype.filter= function(filter, that /*opt*/) {
-        var other= [], v;
-        for (var i=0, n= this.length; i<n; i++)
-            if (i in this && filter.call(that, v= this[i], i, this))
-                other.push(v);
-        return other;
-    };
-}
-if (!('every' in Array.prototype)) {
-    Array.prototype.every= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this && !tester.call(that, this[i], i, this))
-                return false;
-        return true;
-    };
-}
-if (!('some' in Array.prototype)) {
-    Array.prototype.some= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this && tester.call(that, this[i], i, this))
-                return true;
-        return false;
-    };
-}
-
 var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 var ARGUMENT_NAMES = /([^\s,]+)/g;
 function getParamNames(func) {
@@ -331,19 +238,8 @@ function getChromeVersion()
   }
 
 
-if (!Number.prototype.toFixed)
-    Number.prototype.toFixed = function(precision) {
-        var power = Math.pow(10, precision || 0);
-        return String(Math.round(this * power)/power);
-    }
-
 String.prototype.contains = function(it) { 
   return this.indexOf(it) > -1; 
-};
-String.prototype.trim = function() { 
-  var r = this.replace(/^\s+/g,'').replace(/\s+$/g,''); 
-  r = r.replace(/^\n+/g,'').replace(/\n+$/g,''); 
-  return r;
 };
 
 String.prototype.gauche = function(souschaine)
@@ -525,17 +421,6 @@ function base64decode(input)
 
   return output;
 }
-
-
-/*callLater: function(callback, argsArray, interval)
-  {
-    setTimeout( (function($callback, $argsArray) {
-    return function() {
-            $callback.apply(null, $argsArray)
-        };
-    })(callback, argsArray) , interval);
-
-},*/
 
 function callLater(func) 
 {
